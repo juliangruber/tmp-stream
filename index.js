7fcde11240c4ef931c093d6c483ce10a37a69c43
Julian Gruber <julian@juliangruber.com>
2013-05-18 18:45:55 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var duplex = require('duplexer');
+var through = require('through');
+
+var nextTick = typeof setImmediate !== 'undefined'
+  ? setImmediate
+  : process.nextTick;
+
+module.exports = tmp;
+
+function tmp () {
+  var replaced = false;
+  var buf = [];
+  var realStream;
+
+  var input = through(function (chunk) {
+    if (!replaced) {
+      buf.push(chunk);
+      return false;
+    } else {
+      return realStream.write(chunk);
+    }
+  });
+
+  var output = through();
+
+  var dpl = duplex(input, output);
+
+  dpl.replace = function (stream) {
+    realStream = stream;
+    replaced = true;
+    
+    dpl.readable = realStream.readable;
+    dpl.writable = realStream.writable;
+
+    if (realStream.readable) realStream.pipe(output);
+    if (realStream.writable) input.pipe(realStream);
+
+    nextTick(function () {
+      for (var i = 0; i < buf.length; i++) realStream.write(buf[i]);
+      buf = null;
+    });
+  }
+
+  return dpl;
+}
